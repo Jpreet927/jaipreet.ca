145dd6a446bde19a71fda1acc86223c27a22e6f7
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-08-10 17:50:18 -0400

    :art: data for photography page

---
+++ data/photography.ts
@@ -0,0 +1,51 @@
+import { Artwork } from "./design";
+
+// import img1 from "@/assets/images/Photography Images/jaipreet-singh-wHXDRkxKo7o-unsplash.jpg";
+import img1 from "@/assets/images/Photography Images/Optimized/jaipreet-singh-wHXDRkxKo7o-unsplash.webp";
+import img2 from "@/assets/images/Photography Images/jaipreet-singh-S0Y9YqkGR-Q-unsplash.jpg";
+import img3 from "@/assets/images/Photography Images/IMG_8625.jpg";
+import img4 from "@/assets/images/Photography Images/jaipreet-singh-mcSNh20f2qs-unsplash.jpg";
+import img5 from "@/assets/images/Photography Images/jaipreet-singh-2YpUByIPpCc-unsplash.jpg";
+import img6 from "@/assets/images/Photography Images/tokyo-tower.jpg";
+import img7 from "@/assets/images/Photography Images/kyoto.jpg";
+import img8 from "@/assets/images/Photography Images/jaipreet-singh--vD3P6IjmAw-unsplash.jpg";
+import img9 from "@/assets/images/Photography Images/performer.jpg";
+
+export const photos: Artwork[] = [
+    {
+        title: "Fushimi Inari Shrine",
+        src: img1,
+    },
+    {
+        title: "Omoide Yokocho",
+        src: img2,
+    },
+    {
+        title: "Inokashira Park",
+        src: img3,
+    },
+    {
+        title: "High Park",
+        src: img4,
+    },
+    {
+        title: "Shibuya Crossing",
+        src: img5,
+    },
+    {
+        title: "Tokyo Tower",
+        src: img6,
+    },
+    {
+        title: "Kyoto Street",
+        src: img7,
+    },
+    {
+        title: "Deer at Nara Park",
+        src: img8,
+    },
+    {
+        title: "Performer at Ghibli Museum",
+        src: img9,
+    },
+];
--- data/projects.ts
+++ data/projects.ts
@@ -3,14 +3,7 @@ import jtweet from "@/assets/images/Project Thumbnails/Jtweet.jpeg";
 import harmoni from "@/assets/images/Project Thumbnails/Harmoni.jpg";
 import ecommerce from "@/assets/images/Project Thumbnails/Ecommerce.jpg";
 import entertainmenthub from "@/assets/images/Project Thumbnails/Entertainment-Hub.jpg";
-
-interface Project {
-    title: string;
-    description: string;
-    category: string;
-    thumbnail: StaticImageData;
-    link: string;
-}
+import { Project } from "@/components/home/projectcard";
 
 export const projects: Project[] = [
     {
@@ -18,7 +11,7 @@ export const projects: Project[] = [
         description:
             "Jtweet is an interpolation of the popular social media platform 'Twitter' where users can create accounts with their emails, create text and image based posts, interact with other users, and receive a personalized feed with posts from followed users.",
         category: "Web Development",
-        thumbnail: jtweet,
+        src: jtweet,
         link: "https://github.com/Jpreet927/jtweet",
     },
     {
@@ -26,7 +19,7 @@ export const projects: Project[] = [
         description:
             "Harmoni is an AI Based Health Assitant developed in a team of 4 to assist users in tracking their health and fitness progress over time. There is a rising prevalence of cardiovascular diseases such as diabetes and stroke, and our team's commitment is to design a solution to provide an easy, simple product for individuals to take the next step in their journey towards improved health.",
         category: "App Development",
-        thumbnail: harmoni,
+        src: harmoni,
         link: "https://www.youtube.com/watch?v=b8yH6oT6mlI",
     },
     {
@@ -34,7 +27,7 @@ export const projects: Project[] = [
         description:
             "An ecommerce website where users can browse products, add products to their cart, manage item quantity, search for products, sort products by price, and review order details. Designed using Photoshop and Figma and implemented in React.js.",
         category: "Web Development",
-        thumbnail: ecommerce,
+        src: ecommerce,
         link: "https://jpreet927.github.io/ecommerce-site/",
     },
     {
@@ -42,7 +35,7 @@ export const projects: Project[] = [
         description:
             "A web application that displays movie and television data fetched form the TMDB public API. Built with React.js.",
         category: "Web Development",
-        thumbnail: entertainmenthub,
+        src: entertainmenthub,
         link: "https://jpreet927.github.io/ecommerce-site/",
     },
 ];
