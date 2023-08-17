5dfe1746d7d8acb08ed59007021e76a47fbcf7cb
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-08-17 17:41:39 -0400

    :zap: animation adjustments

--- components/home/iconbox.tsx
+++ components/home/iconbox.tsx
@@ -10,17 +10,11 @@ const IconBox = ({
 }) => {
     return (
         <div className="relative 2xl:p-8 xl:p-6 sm:p-10 p-6 bg-slate-700/10 rounded-xl border-white/5 border hover:-translate-y-2 transition-transform ease-in-out duration-500">
+            <Image src={iconPath} alt={iconAlt} objectFit="contain" />
             <Image
                 src={iconPath}
                 alt={iconAlt}
-                // layout="fill"
-                objectFit="contain"
-            />
-            <Image
-                src={iconPath}
-                alt={iconAlt}
-                className="absolute top-0 left-0 blur-xl 2xl:p-8 xl:p-6 sm:p-10 p-6 opacity-40 hover:opacity-70 transition-opacity ease-in-out duration-500"
-                // layout="fill"
+                className="absolute top-0 left-0 blur-xl 2xl:p-8 xl:p-6 sm:p-10 p-6 opacity-60 hover:opacity-90 transition-opacity ease-in-out duration-500"
                 objectFit="contain"
             />
         </div>
