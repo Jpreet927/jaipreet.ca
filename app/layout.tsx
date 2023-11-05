54869a5f196635c9314ac572c7290cd3fe933271
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-11-05 15:19:56 -0500

    fix: :mag: seo improvements and opengraph/twitter-image support

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,39 @@ import "./globals.css";
 export const metadata: Metadata = {
     title: "Jaipreet Singh",
     description: "Jaipreet Singh's personal website.",
+    keywords: [
+        "Jaipreet Singh",
+        "Jai Preet Singh",
+        "Jaipreet",
+        "Jai Preet",
+        "Software",
+        "Developer",
+        "Graphic",
+        "Designer",
+    ],
+    openGraph: {
+        title: "Jaipreet Singh",
+        description: "Jaipreet Singh's personal website.",
+        url: "https://jaipreet.ca",
+        siteName: "Jaipreet Singh",
+        images: [
+            {
+                url: "https://jaipreet.ca/opengraph.jpg",
+                width: 1200,
+                height: 630,
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        site: "@jpreet_",
+        creator: "@jpreet_",
+        title: "Jaipreet Singh",
+        description: "Jaipreet Singh's personal website.",
+        images: "https://jaipreet.ca/opengraph.jpg",
+    },
 };
 
 export default function RootLayout({
