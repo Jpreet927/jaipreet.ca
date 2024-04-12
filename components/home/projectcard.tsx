59b6588769f5fdfb60314f5285854ef8c91cae8d
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-04-12 19:34:46 -0400

    feat: :bento: rearranged photography grid

--- components/home/projectcard.tsx
+++ components/home/projectcard.tsx
@@ -16,38 +16,38 @@ export interface Project {
 
 const ProjectCard = ({ project }: { project: Project }) => {
     return (
-        <motion.div
-            initial={{ opacity: 0, y: 15 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-                duration: 1,
-                delay: 0.5,
-                ease: "easeOut",
-            }}
-            viewport={{ once: true }}
-            className="relative group overflow-hidden aspect-video w-full"
-        >
-            <div className=" absolute bottom-0 left-0 h-[75%] w-full bg-gradient-to-t from-black to-black-0 opacity-50 group-hover:opacity-90 transition ease-out duration-500"></div>
-            <div className="absolute bottom-0 left-0 p-4 w-full flex flex-col gap-1 group">
-                <p className="text-[0.6rem] tracking-[0.2rem] opacity-0 translate-y-10 group-hover:opacity-100 group-hover:translate-y-0 transition ease-in-out duration-700">
-                    {project.category.toUpperCase()}
-                </p>
-                <Link href={project.link} target="_blank">
+        <Link href={project.link} target="_blank">
+            <motion.div
+                initial={{ opacity: 0, y: 15 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                    duration: 1,
+                    delay: 0.5,
+                    ease: "easeOut",
+                }}
+                viewport={{ once: true }}
+                className="relative group overflow-hidden aspect-video w-full"
+            >
+                <div className=" absolute bottom-0 left-0 h-[75%] w-full bg-gradient-to-t from-black to-black-0 opacity-50 group-hover:opacity-90 transition ease-out duration-500"></div>
+                <div className="absolute bottom-0 left-0 p-4 w-full flex flex-col gap-1 group">
+                    <p className="text-[0.6rem] tracking-[0.2rem] opacity-0 translate-y-10 group-hover:opacity-100 group-hover:translate-y-0 transition ease-in-out duration-700">
+                        {project.category.toUpperCase()}
+                    </p>
                     <div className="flex justify-between translate-y-10 group-hover:translate-y-0 transition ease-in-out duration-700">
                         <h3 className="text-xl font-bold">{project.title}</h3>
                         <ArrowOutwardIcon />
                     </div>
-                </Link>
-                <p className="text-xs opacity-0 translate-y-10 group-hover:opacity-75 group-hover:translate-y-0 transition ease-in-out duration-700">
-                    {project.description}
-                </p>
-            </div>
-            <Image
-                src={project.src}
-                alt={project.title}
-                style={{ objectFit: "cover", height: "100%" }}
-            />
-        </motion.div>
+                    <p className="text-xs opacity-0 translate-y-10 group-hover:opacity-75 group-hover:translate-y-0 transition ease-in-out duration-700">
+                        {project.description}
+                    </p>
+                </div>
+                <Image
+                    src={project.src}
+                    alt={project.title}
+                    style={{ objectFit: "cover", height: "100%" }}
+                />
+            </motion.div>
+        </Link>
     );
 };
 
--- data/photography.ts
+++ data/photography.ts
@@ -11,6 +11,10 @@ import img8 from "@/assets/images/Photography Images/jaipreet-singh--vD3P6IjmAw-
 import img9 from "@/assets/images/Photography Images/performer.jpg";
 import img10 from "@/assets/images/Photography Images/IMG_0141.jpg";
 import img11 from "@/assets/images/Photography Images/kyoto-street.jpg";
+import img12 from "@/assets/images/Photography Images/IMG_9709.jpg";
+import img13 from "@/assets/images/Photography Images/jaipreet-singh-_4QKa5tcHl4-unsplash.jpg";
+import img14 from "@/assets/images/Photography Images/jaipreet-singh-i7OR6QklR4s-unsplash.jpg";
+import img15 from "@/assets/images/Photography Images/jaipreet-singh-uUyaweG20C8-unsplash.jpg";
 
 export const photos: Artwork[] = [
     {
@@ -18,43 +22,55 @@ export const photos: Artwork[] = [
         src: img1,
     },
     {
-        title: "Inokashira Park",
-        src: img3,
-    },
-    {
         title: "High Park",
         src: img4,
-    },
-    {
-        title: "Omoide Yokocho",
-        src: img2,
-    },
-    {
-        title: "Shibuya Crossing",
-        src: img5,
     },
     {
         title: "Tokyo Tower",
         src: img6,
     },
     {
-        title: "Kyoto Street",
-        src: img7,
+        title: "Shibuya Crossing",
+        src: img5,
     },
     {
         title: "Deer at Nara Park",
         src: img8,
     },
     {
+        title: "Inokashira Park",
+        src: img3,
+    },
+    {
+        title: "Kinkaku-ji Golden Temple in Kyoto",
+        src: img14,
+    },
+    {
+        title: "Omoide Yokocho",
+        src: img2,
+    },
+    {
         title: "Performer at Ghibli Museum",
         src: img9,
     },
     {
-        title: "Stairs at Kyoto Temple",
-        src: img10,
-    },
-    {
         title: "Kyoto Street",
         src: img11,
+    },
+    {
+        title: "Architecture in Kyoto Station",
+        src: img15,
+    },
+    {
+        title: "Osaka Castle",
+        src: img12,
+    },
+    {
+        title: "A Busy Street in Mikata, Tokyo",
+        src: img13,
+    },
+    {
+        title: "Stairs at Kyoto Temple",
+        src: img10,
     },
 ];
