c2c4b87d9cc09583b2a9778d14fa8ab02650bdad
Jaipreet Singh <jaipreetbsingh@gmail.com>
2025-12-23 15:57:11 -0500

    chore: :bento: updated photography page assets

--- data/photography.ts
+++ data/photography.ts
@@ -1,76 +1,85 @@
 import { Artwork } from "./design";
 
-import img1 from "@/assets/images/Photography Images/jaipreet-singh-wHXDRkxKo7o-unsplash.jpg";
-import img2 from "@/assets/images/Photography Images/jaipreet-singh-S0Y9YqkGR-Q-unsplash.jpg";
-import img3 from "@/assets/images/Photography Images/IMG_8625.jpg";
-import img4 from "@/assets/images/Photography Images/jaipreet-singh-mcSNh20f2qs-unsplash.jpg";
-import img5 from "@/assets/images/Photography Images/jaipreet-singh-2YpUByIPpCc-unsplash.jpg";
-import img6 from "@/assets/images/Photography Images/tokyo-tower.jpg";
-import img7 from "@/assets/images/Photography Images/kyoto.jpg";
-import img8 from "@/assets/images/Photography Images/jaipreet-singh--vD3P6IjmAw-unsplash.jpg";
-import img9 from "@/assets/images/Photography Images/performer.jpg";
-import img10 from "@/assets/images/Photography Images/IMG_0141.jpg";
-import img11 from "@/assets/images/Photography Images/kyoto-street.jpg";
-import img12 from "@/assets/images/Photography Images/IMG_9709.jpg";
-import img13 from "@/assets/images/Photography Images/jaipreet-singh-_4QKa5tcHl4-unsplash.jpg";
-import img14 from "@/assets/images/Photography Images/jaipreet-singh-i7OR6QklR4s-unsplash.jpg";
-import img15 from "@/assets/images/Photography Images/jaipreet-singh-uUyaweG20C8-unsplash.jpg";
+import fushimiInari from "@/assets/images/Photography Images/jaipreet-singh-wHXDRkxKo7o-unsplash.jpg";
+import omoideYokocho from "@/assets/images/Photography Images/jaipreet-singh-S0Y9YqkGR-Q-unsplash.jpg";
+import inokashiraPark from "@/assets/images/Photography Images/IMG_8625.jpg";
+import highPark from "@/assets/images/Photography Images/jaipreet-singh-mcSNh20f2qs-unsplash.jpg";
+import shibuyaCrossing from "@/assets/images/Photography Images/jaipreet-singh-2YpUByIPpCc-unsplash.jpg";
+import naraDeer from "@/assets/images/Photography Images/jaipreet-singh--vD3P6IjmAw-unsplash.jpg";
+import osakaCastle from "@/assets/images/Photography Images/IMG_9709.jpg";
+import mikata from "@/assets/images/Photography Images/jaipreet-singh-_4QKa5tcHl4-unsplash.jpg";
+import kinkakuji from "@/assets/images/Photography Images/jaipreet-singh-i7OR6QklR4s-unsplash.jpg";
+import tokyoTower from "@/assets/images/Photography Images/jaipreet-singh-9i9gpJ2BZXk-unsplash.jpg";
+import arashiyamaBoat from "@/assets/images/Photography Images/jaipreet-singh-tRRgw0l6tm0-unsplash.jpg";
+import lagunaBeach from "@/assets/images/Photography Images/jaipreet-singh-McQg_SNWGLQ-unsplash.jpg";
+import wakayamaBeach from "@/assets/images/Photography Images/jaipreet-singh-8BXhVjdz_w8-unsplash.jpg";
+import kyoto from "@/assets/images/Photography Images/jaipreet-singh-4LbJpP5_iWM-unsplash.jpg";
+import kamakura from "@/assets/images/Photography Images/jaipreet-singh-_yuvScQNcqU-unsplash.jpg";
+import kamakuraShrine from "@/assets/images/Photography Images/jaipreet-singh-4L8gEKwGvHM-unsplash.jpg";
 
 export const photos: Artwork[] = [
-    {
-        title: "Fushimi Inari Shrine",
-        src: img1,
-    },
-    {
-        title: "High Park",
-        src: img4,
-    },
-    {
-        title: "Tokyo Tower",
-        src: img6,
-    },
-    {
-        title: "Shibuya Crossing",
-        src: img5,
-    },
-    {
-        title: "Deer at Nara Park",
-        src: img8,
-    },
-    {
-        title: "Inokashira Park",
-        src: img3,
-    },
-    {
-        title: "Kinkaku-ji Golden Temple in Kyoto",
-        src: img14,
-    },
-    {
-        title: "Omoide Yokocho",
-        src: img2,
-    },
-    {
-        title: "Performer at Ghibli Museum",
-        src: img9,
-    },
-    {
-        title: "Kyoto Street",
-        src: img11,
-    },
-    {
-        title: "Architecture in Kyoto Station",
-        src: img15,
-    },
-    {
-        title: "Osaka Castle",
-        src: img12,
-    },
-    {
-        title: "A Busy Street in Mikata, Tokyo",
-        src: img13,
-    },
-    {
-        title: "Stairs at Kyoto Temple",
-        src: img10,
-    },
+  {
+    title: "Fushimi Inari Shrine",
+    src: fushimiInari,
+  },
+  {
+    title: "Shibuya Crossing",
+    src: shibuyaCrossing,
+  },
+  {
+    title: "High Park",
+    src: highPark,
+  },
+  {
+    title: "Kinkaku-ji Golden Temple in Kyoto",
+    src: kinkakuji,
+  },
+  {
+    title: "Laguna Beach",
+    src: lagunaBeach,
+  },
+  {
+    title: "Omoide Yokocho",
+    src: omoideYokocho,
+  },
+  {
+    title: "Wakayama Beach",
+    src: wakayamaBeach,
+  },
+  {
+    title: "Kamakura Shrine",
+    src: kamakuraShrine,
+  },
+  {
+    title: "Kyoto Tower",
+    src: kyoto,
+  },
+  {
+    title: "Deer at Nara Park",
+    src: naraDeer,
+  },
+  {
+    title: "Inokashira Park",
+    src: inokashiraPark,
+  },
+  {
+    title: "Tokyo Tower",
+    src: tokyoTower,
+  },
+  {
+    title: "Couple in Arashiyama",
+    src: arashiyamaBoat,
+  },
+  {
+    title: "Kamakura",
+    src: kamakura,
+  },
+  {
+    title: "Osaka Castle",
+    src: osakaCastle,
+  },
+  {
+    title: "A Busy Street in Mikata, Tokyo",
+    src: mikata,
+  },
 ];
